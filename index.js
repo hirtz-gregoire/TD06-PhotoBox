7af51e6c0668139a455279b52ef07613dabbf7d9
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2024-06-11 19:33:46 +0200

    exercice3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import photoloader from "./lib/photoloader.js"
-import ui from "./lib/ui.js"
+import ui from "./lib/ui/ui.js"
+import gallery from "./lib/gallery";
+import gallery_ui from "./lib/ui/gallery_ui";
 
 function getPicture( id ){
     if (typeof(id)!=="number" || id<=0)
@@ -7,7 +9,7 @@ function getPicture( id ){
 
     photoloader.loadPicture(id).then((data) => {
         let photo = data.photo;
-        //console.log(photo.titre+" ("+photo.type+")\n"+photo.url.href)
+        console.log(photo.titre+" ("+photo.type+")\n"+photo.url.href)
         ui.displayPicture(data)
         getCategorie(data).then((data) => {
             ui.displayCategorie(data);
@@ -15,11 +17,10 @@ function getPicture( id ){
         getCommentaires(data).then((data) => {
             ui.displayCommentaires(data);
         })
-
     })
 }
 
-getPicture(window.location.hash ? parseInt(window.location.hash.substr(1)): 105);
+//getPicture(window.location.hash ? parseInt(window.location.hash.substr(1)): 105);
 
 
 
@@ -35,5 +36,38 @@ function getCommentaires(image){
     return photoloader.loadResource(image.links.comments.href);
 }
 
+
+function load(){
+    let p = gallery.load();
+    p.then((data) => {
+        gallery_ui.display_galerie(data);
+    })
+}
+
+document.getElementById("load").addEventListener("click", function(){
+    load()
+})
+
+document.getElementById("next").addEventListener("click", function(){
+    gallery.next();
+    load();
+})
+
+document.getElementById("prev").addEventListener("click", function(){
+    gallery.prev();
+    load();
+})
+
+document.getElementById("first").addEventListener("click", function(){
+    gallery.first();
+    load();
+})
+
+document.getElementById("last").addEventListener("click", function(){
+    gallery.last();
+    load();
+})
+
+document.onload = load();
 
 
