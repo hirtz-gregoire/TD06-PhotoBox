96f5a64105477d65e409134c262310e45770289b
GregoireHirtz <hirtz.gregoire@ghmail.fr>
2024-06-11 10:38:24 +0200

    exercice1 etape2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import photoloader from "./lib/photoloader.js"
+import ui from "./lib/ui.js"
 
 function getPicture( id ){
     if (typeof(id)!=="number" || id<=0)
@@ -6,11 +7,13 @@ function getPicture( id ){
 
     photoloader.loadPicture(id).then((data) => {
         let photo = data.photo;
-        console.log(photo.titre+" ("+photo.type+")\n"+photo.url.href)
+        //console.log(photo.titre+" ("+photo.type+")\n"+photo.url.href)
+        ui.displayPicture(data)
     })
 }
 
-getPicture(window.location.hash ? window.location.hash.substr(1): 105);
+getPicture(window.location.hash ? parseInt(window.location.hash.substr(1)): 105);
+
 
 
 
